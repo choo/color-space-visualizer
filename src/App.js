4e20b10e2751afd09d5a2b33bd98c1d31ccb7fb6
choo <choo.hacks@gmail.com>
2019-12-30 21:57:50 +0900

    Move style definition to style.js from App.js

--- src/App.js
+++ src/App.js
@@ -4,68 +4,9 @@ import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 
-import { makeStyles } from '@material-ui/core/styles';
+import useStyles from './style.js'
 
 import ThreeColorSpace from './components/ThreeColorSpace';
-
-const SPACING = {
-  'sm': 1,
-  'md': 2,
-  'lg': 2,
-};
-const logoSize = {
-  'sm': 48,
-  'md': 56,
-  'lg': 64,
-};
-
-const makeHeaderStyle = (theme, space) => {
-  return {
-    top: theme.spacing(space),
-    left: theme.spacing(space),
-    //right: theme.spacing(space),
-  };
-};
-const makeLogoStyle = (height) => {
-  return {
-    height: height,
-  };
-};
-const makeButtonsStyle = (theme, space) => {
-  return {
-    bottom: theme.spacing(space),
-    left: theme.spacing(space),
-  };
-};
-
-const useStyles = makeStyles(theme => ({
-  header: {
-    position: 'absolute',
-    flexGrow: 1,
-    display: 'flex',
-    [theme.breakpoints.down('sm')]: makeHeaderStyle(theme, SPACING.sm),
-    [theme.breakpoints.up('sm')]  : makeHeaderStyle(theme, SPACING.md),
-    [theme.breakpoints.up('lg')]  : makeHeaderStyle(theme, SPACING.lg),
-  },
-  logo: {
-    [theme.breakpoints.down('sm')]: makeLogoStyle(logoSize.sm),
-    [theme.breakpoints.up('sm')]  : makeLogoStyle(logoSize.md),
-    [theme.breakpoints.up('lg')]  : makeLogoStyle(logoSize.lg),
-  },
-  buttons: {
-    position: 'absolute',
-    [theme.breakpoints.down('sm')]: makeButtonsStyle(theme, SPACING.sm),
-    [theme.breakpoints.up('sm')]  : makeButtonsStyle(theme, SPACING.md),
-    [theme.breakpoints.up('lg')]  : makeButtonsStyle(theme, SPACING.lg),
-  },
-  colorPreview: {
-    position: 'absolute',
-    width: '100px',
-    height: '100px',
-    bottom: theme.spacing(2),
-    right: theme.spacing(2),
-  },
-}));
 
 
 const App = (props) => {
