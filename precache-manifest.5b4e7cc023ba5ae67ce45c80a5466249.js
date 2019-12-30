f62ee8050535f591ba0a51b03786d098dbf8e8b6
choo <sugimori.choo@gmail.com>
2019-12-30 17:03:20 +0900

    Updates

---
+++ precache-manifest.5b4e7cc023ba5ae67ce45c80a5466249.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64227569cf3a1e4b2351191bf3fee890",
+    "url": "/color-space-visualizer/index.html"
+  },
+  {
+    "revision": "f928482f88af9d135e56",
+    "url": "/color-space-visualizer/static/js/2.bbabb180.chunk.js"
+  },
+  {
+    "revision": "8b9700849dd6935184db635a683efc24",
+    "url": "/color-space-visualizer/static/js/2.bbabb180.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7921700ae5d6fcda2a5b",
+    "url": "/color-space-visualizer/static/js/main.058ec3ff.chunk.js"
+  },
+  {
+    "revision": "95296484d09f936fc7ba",
+    "url": "/color-space-visualizer/static/js/runtime-main.01576c8c.js"
+  }
+]);
