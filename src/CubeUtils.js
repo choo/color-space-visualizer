0a54fe5812b91b602bdf0d519bb99f778dee8a67
choo <choo.hacks@gmail.com>
2019-12-30 11:24:35 +0900

    Refactor mesh config

--- src/CubeUtils.js
+++ src/CubeUtils.js
@@ -5,6 +5,8 @@ export const createCubeMesh = (size, color, x, y, z) => {
   const geometry = new THREE.CubeGeometry(size, size, size);
   const material = new THREE.MeshPhongMaterial({color: color});
   const mesh = new THREE.Mesh(geometry, material);
+  mesh.castShadow = true;
+  mesh.receiveShadow = true;
   mesh.position.set(x, y, z);
   return mesh;
 };
