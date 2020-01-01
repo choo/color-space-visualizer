bd3577e59aa255286dab4a48e03f4bb395237399
choo <sugimori.choo@gmail.com>
2020-01-01 09:38:37 +0900

    Modify imporing statement

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, { useState } from 'react';
-import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
-import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
-import {selectTextColor} from './ColorUtils';
 
 import useStyles from './style.js';
-
 import ThreeColorSpace from './components/ThreeColorSpace';
+import {selectTextColor} from './ColorUtils';
 
 
 const App = (props) => {
